21b6cb553641384d6026adae042e04ad2e0463ba
Guillermo Gonzalez G. <j.guillermo.glez+git@gmail.com>
2025-10-15 20:26:08 -0600

    Creacion de Pruebas unitarias desde entorno de pruebas aparte del entorno de desarrollo

---
+++ Testing/UnitTests/prueba_financial.py
@@ -0,0 +1,26 @@
+from Calculus.FinancialCalculus import DenominacionMoneda as denominacion
+
+monedas = [10,5,2,1,0.50]
+cantidadMonedas = [100,100,100,100,100]
+billetes = [1000,500,200,100,50,20]
+cantidadBilletes = [100,100,100,100,100,100]
+nombre = "peso"
+pais = "Mexico"
+
+MXN = denominacion.divisa(monedas,billetes,nombre,pais)
+cajaMXN = denominacion.caja(MXN,cantidadMonedas,cantidadBilletes)
+cambio = denominacion.generaCambio(100,122.50,cajaMXN)
+print(cambio)
+
+monedas = [0.50,0.25,0.10,0.05,0.01]
+cantidadMonedas = [100,100,100,100,100]
+billetes = [100,50,20,5,2,1]
+cantidadMonedas = [100,100,100,100,100,100]
+nombre = "dolar"
+pais = "USA"
+
+USD = denominacion.divisa(monedas,billetes,nombre,pais)
+
+
+print(MXN)
+print(USD)
